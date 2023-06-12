8179c209d3145ef55d953539b523c7236af90a46
Mammetnazar <nazar.rudn@gmail.com>
2023-06-12 22:14:51 +0300

    - added constants.swift - added necessary keys and other information

---
+++ ImageFeed/Resources/Constants.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let AccessKey = "JsM9Imkj0s3xxVY-TDO_5cwTgdCookHSfIdCecZ6WeE"
+let SecretKey = "eqZxIvbiAfzA7aMqPxcVf8s5BO3Sc73iC0ZJu_9wsOI"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+
+
+
+
