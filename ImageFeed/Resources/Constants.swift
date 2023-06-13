dac70978cf8776d998aa70a5836ce6d3a2d6fb0b
Mammetnazar <nazar.rudn@gmail.com>
2023-06-14 02:54:26 +0300

    - added AuthViewController - made it look like in the task, except initial state of button (might need change the button in the future)

--- ImageFeed/Resources/Constants.swift
+++ ImageFeed/Resources/Constants.swift
@@ -5,7 +5,3 @@ let SecretKey = "eqZxIvbiAfzA7aMqPxcVf8s5BO3Sc73iC0ZJu_9wsOI"
 let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let AccessScope = "public+read_user+write_likes"
 let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
-
-
-
-
