8baa09788d7b5fffa62618a1fe10e0e399ac9673
Mammetnazar <nazar.rudn@gmail.com>
2023-06-14 03:31:13 +0300

    - added didTapButton - changed the button to match requested style

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,4 +5,11 @@ final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
     //something
+    
+    @IBAction private func didTapBackButton(_ Sender: Any?) {
+        //do something
+        
+    }
+    
+    
 }
